bc9127437ce8278a40ded8e47362548847c73565
carmanti <mantilla.carlosjoselo@gmail.com>
2024-09-29 17:50:34 -0500

    Format code

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,11 +1,15 @@
 public class Vehiculo {
     private String placa;
 
-    public Vehiculo(String placa){
-        this.placa =placa;
+    public Vehiculo(String placa) {
+        this.placa = placa;
     }
 
-    public String getUltimoDigito(){
-        return placa.substring(placa.length()-1);
+    public String getUltimoDigito() {
+        return placa.substring(placa.length() - 1);
+    }
+
+    public String getPlaca() {
+        return placa;
     }
 }
